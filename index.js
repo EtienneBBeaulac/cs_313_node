f62acb5d3c9e01391f107793d06879e866cab0b4
Etienne Beaulac <etiennebeaulac@gmail.com>
2019-06-18 16:49:17 -0600

    team 09

--- index.js
+++ index.js
@@ -1,10 +1,47 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const app = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 5000;
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app()
+  .use(app.static(path.join(__dirname, "public")))
+  .set("views", path.join(__dirname, "views"))
+  .set("view engine", "ejs")
+  .get("/", (req, res) => res.render("pages/index"))
+  .get("/assignments", (req, res) => res.render("pages/assignments"))
+  .get("/team-activities", (req, res) => res.render("pages/team-activities"))
+  .get("/team-activities/09", (req, res) =>
+    res.render("pages/team-activities/team-09/main")
+  )
+  .get("/team-activities/09/math", getMath)
+  .get("/team-activities/09/math_service", sendMath)
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+function getMath(req, res) {
+  let value = doMath(req.query);
+  res.render("pages/team-activities/team-09/math", { value: value });
+}
+
+function sendMath(req, res) {
+  let value = doMath(req.query);
+  res.json({ result: value })
+}
+
+function doMath(query) {
+  let operand = query.operand;
+  let num1 = parseFloat(query.num1);
+  let num2 = parseFloat(query.num2);
+  let value = 0;
+
+  if (operand === "add") {
+    value = num1 + num2;
+  } else if (operand === "sub") {
+    value = num1 - num2;
+  } else if (operand === "mult") {
+    value = num1 * num2;
+  } else if (operand === "div") {
+    value = num1 / num2;
+  }
+  return value;
+}
+
+
